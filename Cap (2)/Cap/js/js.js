c549ee89f390e04f9788f6efed911634abf4cbe4
rcristobal95 <108343744+rcristobal95@users.noreply.github.com>
2022-06-28 20:41:32 +0800

    Add files via upload

---
+++ Cap (2)/Cap/js/js.js
@@ -0,0 +1,7 @@
+const element = document.getElementById("myBtn");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+  window.alert("adsadsdsd");
+}
